165ee689d3a22a0af421e7e491eba7d08a651089
Veirt <55097092+Veirt@users.noreply.github.com>
2023-07-27 21:47:46 +0800

    build(vite): use Split Vendor Chunk strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), splitVendorChunkPlugin()],
 });
