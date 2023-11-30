57d11f0af74a67e99faa66f4497483e385cf52c7
Veirt <vveirt@gmail.com>
2023-11-30 14:55:39 +0800

    build: add brotli compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-    plugins: [sveltekit(), splitVendorChunkPlugin()],
+    plugins: [
+        sveltekit(),
+        splitVendorChunkPlugin(),
+        compression({ algorithm: "brotliCompress" }),
+    ],
 });
