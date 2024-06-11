27d46674958ded7eaee4fd686f3e253324b1768a
Veirt <veirt@duck.com>
2024-06-11 23:18:59 +0800

    perf: use is-reachable

--- src/lib/components/Indicator.svelte
+++ src/lib/components/Indicator.svelte
@@ -17,7 +17,7 @@
 {#await fetchPing()}
     <div class="w-2 h-2 visibility-hidden" />
 {:then data}
-    {#if data.statusCode < 500}
+    {#if data.status}
         <div class="self-end mr-4 w-2 h-2 bg-green-500 rounded-full" />
     {:else}
         <div class="self-end mr-4 w-2 h-2 bg-red-500 rounded-full" />
--- src/routes/api/ping/+server.ts
+++ src/routes/api/ping/+server.ts
@@ -1,34 +1,13 @@
 import { getWidgetInfo } from "$lib/utils/widget";
 import { json, type RequestHandler } from "@sveltejs/kit";
-import axios from "axios";
-import https from "https";
-
-const instance = axios.create({
-    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-    validateStatus() {
-        return true;
-    },
-});
-
-async function checkWebsiteStatus(pingUrl: string) {
-    let response = await instance.head(pingUrl);
-    if (response.status === 501) {
-        // Use GET if HEAD is not implemented
-        response = await instance.get(pingUrl);
-    }
-
-    return response.status;
-}
+import isReachable from "is-reachable";
 
 export const GET = (async ({ url }) => {
     const group = url.searchParams.get("group")!;
     const title = url.searchParams.get("title")!;
     const { url: pingUrl } = getWidgetInfo(group, title).ping!;
 
-    try {
-        const statusCode = await checkWebsiteStatus(pingUrl);
-        return json({ statusCode });
-    } catch (error) {
-        return json({ error });
-    }
+    const status = await isReachable(pingUrl);
+
+    return json({ status });
 }) satisfies RequestHandler;
